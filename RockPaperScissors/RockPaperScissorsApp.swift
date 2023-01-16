6e0d0c14e681fadab0567f81dc812c722bb65f4b
soulFood5632 <loganu@shaw.ca>
2023-01-15 20:59:29 -0800

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Logan Underwood on 2023-01-15.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
